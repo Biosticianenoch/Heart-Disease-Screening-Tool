597b46e4dd082ea39fd53399b72a30fcec45fefc
Enock Wafula Bereka <enochosenwafulah@gmail.com>
2025-05-14 23:19:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('🎈 Heart Disease Screening Tool')
 
-st.write('This Tool is used to assess the heart disease status of individual using machine learning integration!')
+st.write('This Tool is used to assess the heart disease status of individuals using machine learning integration!')
