eac38027086d5b4522b618dd578ad907735a8db8
Enock Wafula Bereka <enochosenwafulah@gmail.com>
2025-05-14 23:32:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
+
 
 st.title('🎈 Heart Disease Screening Tool')
 
 st.write('This Tool is used to assess the heart disease status of individuals using machine learning integration!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/Biosticianenoch/data/refs/heads/main/heart.csv')
+df 
